32d9a2f06db393002b2d4ccf2a248640f7832d69
YashKsharma312 <112351428+YashKsharma312@users.noreply.github.com>
2023-01-12 15:25:39 +0530

    Delete Temprature_converter/target directory

--- Temprature_converter/target/debug/Temprature_converter.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Lenovo\OneDrive\Desktop\Rust\Temprature_converter\target\debug\Temprature_converter.exe: C:\Users\Lenovo\OneDrive\Desktop\Rust\Temprature_converter\src\main.rs
--- Temprature_converter/target/debug/deps/Temprature_converter.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Lenovo\OneDrive\Desktop\Rust\Temprature_converter\target\debug\deps\Temprature_converter.exe: src\main.rs
-
-C:\Users\Lenovo\OneDrive\Desktop\Rust\Temprature_converter\target\debug\deps\Temprature_converter.d: src\main.rs
-
-src\main.rs:

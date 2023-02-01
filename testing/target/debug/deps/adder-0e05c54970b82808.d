c59683cb07a270b148f9887ac5c8f49258d2f2d3
YashKsharma312 <112351428+YashKsharma312@users.noreply.github.com>
2023-02-01 12:20:40 +0530

    init commit

---
+++ testing/target/debug/deps/adder-0e05c54970b82808.d
@@ -0,0 +1,5 @@
+C:\Users\Lenovo\OneDrive\Desktop\Rust\adder\target\debug\deps\adder-0e05c54970b82808.exe: src\lib.rs
+
+C:\Users\Lenovo\OneDrive\Desktop\Rust\adder\target\debug\deps\adder-0e05c54970b82808.d: src\lib.rs
+
+src\lib.rs:
---
+++ testing/target/debug/deps/adder-1bb7a7c51071cdcb.d
@@ -0,0 +1,7 @@
+C:\Users\Lenovo\OneDrive\Desktop\Rust\adder\target\debug\deps\adder-1bb7a7c51071cdcb.rmeta: src\lib.rs
+
+C:\Users\Lenovo\OneDrive\Desktop\Rust\adder\target\debug\deps\libadder-1bb7a7c51071cdcb.rlib: src\lib.rs
+
+C:\Users\Lenovo\OneDrive\Desktop\Rust\adder\target\debug\deps\adder-1bb7a7c51071cdcb.d: src\lib.rs
+
+src\lib.rs:
